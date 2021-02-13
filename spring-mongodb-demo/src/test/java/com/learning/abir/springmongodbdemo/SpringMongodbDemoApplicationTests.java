f0ef6f804651699c5a4a4e70924e4fe7bc1e0569
OuniAbir <ouniabir118@gmail.com>
2021-02-13 13:12:35 +0100

    intial commit - project generation

---
+++ spring-mongodb-demo/src/test/java/com/learning/abir/springmongodbdemo/SpringMongodbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.abir.springmongodbdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongodbDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
